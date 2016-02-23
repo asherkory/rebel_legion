c0829817b75d46a7c0edd53f1aaddad15c06a9b6
shinyrachel <rachel.kory@gmail.com>
2016-02-23 18:22:35 -0500

    scrapes costume pages

--- lib/rebellegion/scraper.rb
+++ lib/rebellegion/scraper.rb
@@ -1,14 +1,19 @@
 require "pry"
 require "open-uri"
 require_relative "../rebellegion.rb"
+require_relative "./costume_category.rb"
+require_relative "./costume.rb"
 
 class RebelLegion::Scraper
-  attr_accessor :categories
+  attr_accessor :categories, :costume_pages
 
   def initialize
     @categories = {}
+    @costume_pages = {}
     get_categories("http://www.rebellegion.com/costume-standards/by-category/")
     make_costume_categories
+    get_costumes
+    # make_costume
   end
 
   def get_categories(url) # gets the main costume category list
@@ -24,13 +29,16 @@ class RebelLegion::Scraper
     end
   end
 
-  def get_costumes # gets a costume page
-    RebelLegion::CostumeCategory.all.each do |name, url|
-
+  def get_costumes # collects titles & urls of costume pages
+    RebelLegion::CostumeCategory.all.each do |costume_category|
+      doc = Nokogiri::HTML(open(costume_category.url))
+      doc.css("div#left-area article.entry-content.clearfix div.et_pt_blogentry.clearfix").each do |costume|
+        costume_pages[costume.css("h2.et_pt_title a").text] = costume.css("h2.et_pt_title a").attribute("href").value
+      end
     end
   end
 
-  def make_costume(data) # sends costume data to the Costume class
+  def make_costume # sends costume data to the Costume class: costume.name will be from costume_pages key, details scraped from url (value)
   end
 end
 
